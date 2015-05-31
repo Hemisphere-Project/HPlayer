a56d771a870ca1ed8dd07f5ba54935705ae28f2a
tpltnt <tpltnt.github@dropcut.net>
2015-05-31 12:50:59 +0200

    initial docs for 'imgPlayer'

--- src/imgPlayer.h
+++ src/imgPlayer.h
@@ -6,16 +6,16 @@
 #include "ofFbo.h"
 
 struct dimsIMG_t {
-    int height;
-    int width;
-    int marginX;
-    int marginY;
+    int height; /*!< height of the display (screen) */
+    int width; /*!< width of the display (screen) */
+    int marginX; /*!< horizontal display margin */
+    int marginY; /*!< vertical display margin */
 };
 
 struct paramsIMG_t {
-    int     blur;
-    int     zoom;
-    bool textured;
+    int     blur; /*!< indicate amount of blur */
+    int     zoom; /*!< level of zoom (in percent) */
+    bool textured; /*!< indicate use of effects */
 };
 
 class imgPlayer 
@@ -27,31 +27,33 @@ class imgPlayer
         imgPlayer();
         void init(bool textured);
         void show();
-        
+
         //CONTROL
         void play(string file);
         void stop();
 
         void setZoom(int zoom);
         void setBlur(int blur);
-        
+
         //STATE
         bool    isPlaying();
 
-        
+
     private:
-                           
+
         void makeRect();
         void clearscreen();
-        
+
         //FX
         void blur();
-        
-        //BUFFER & SHADER
-        ofShader blurH, blurV;
-        ofFbo framebuffer, frameblur;   
 
-        ofImage* player;
-        dimsIMG_t          dim;
-        paramsIMG_t        params; 
+        //BUFFER & SHADER
+        ofShader blurH; /*!< shader for horizontal blurring */
+        ofShader  blurV; /*!< shader for vertical blurring */
+        ofFbo framebuffer; /*!< basic framebuffer */
+        ofFbo  frameblur; /*!< framebuffer for blurring */
+
+        ofImage* player; /*!< container class for image (present during playback) */
+        dimsIMG_t          dim; /*!< screen/display dimensions */
+        paramsIMG_t        params; /*!< parameters for displaying images */
 };
